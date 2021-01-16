a88bb6c8318eb1f630ded5e3bf5c84e6dae2d4df
Kamran Elchuzade <abbox@Kamrans-iMac.local>
2021-01-16 14:47:14 +0300

    test

--- Assets/Scripts/HomeStatus.cs
+++ Assets/Scripts/HomeStatus.cs
@@ -118,7 +118,6 @@ public class HomeStatus : MonoBehaviour
         {
             hintButton.SetActive(false);
         }
-
         // Set the ball based on which ball index is selected in player data
         SetBallPrefab();
         // Set the background based on the ball
